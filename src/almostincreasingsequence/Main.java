1133a8c0fde9242e6777ab8aa00850640f96e8b5
uchario <uchario@yahoo.com>
2023-01-23 03:47:05 +0100

    feat: add "gopinathSolution" method

--- src/almostincreasingsequence/Main.java
+++ src/almostincreasingsequence/Main.java
@@ -107,8 +107,24 @@ public class Main {
             return true;
 
         return false;
-
     }
+
+    public static boolean gopinathSolution(int[] sequence) {
+        int numErr = 0;
+        for (int i = 0; i < sequence.length - 1; i++) {
+            if (sequence[i] - sequence[i+1] >= 0) {
+                numErr += 1;
+                if (i - 1 >= 0 && i + 2 <= sequence.length - 1
+                        && sequence[i] - sequence[i+2] >= 0
+                        && sequence[i-1] - sequence[i+1] >= 0) {
+                    return false;
+                }
+            }
+        }
+
+        return numErr <= 1;
+    }
+
 
 
 }
