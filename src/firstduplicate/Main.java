acd617d91d5ebc620aaafbfa817678154f45dc0e
uchario <uchario@yahoo.com>
2023-01-29 20:49:33 +0100

    feat: add marinisaac1Solution of "firstDuplicate" method and package

--- src/firstduplicate/Main.java
+++ src/firstduplicate/Main.java
@@ -69,4 +69,14 @@ public class Main {
         }
         return finalNumber;
     }
+
+    public static int marinisaac1Solution(int[] a) {
+        for(int i = 0; i < a.length; i++)
+            if(a[Math.abs(a[i]) - 1] < 0)
+                return Math.abs(a[i]);
+            else{
+                a[Math.abs(a[i]) - 1] =- a[Math.abs(a[i]) - 1];
+            }
+        return -1;
+    }
 }
