720e64ab03b71bf4123cbf728aa700303e5c85f1
uchario <uchario@yahoo.com>
2023-01-29 23:41:03 +0100

    feat: add "marinisaac1Solution" to firstDuplicate problem

--- src/firstduplicate/Main.java
+++ src/firstduplicate/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         int[] array = {1, 1, 2, 2, 1};
-        int finalNumber = firstDuplicate(array);
+        int finalNumber = marinisaac1Solution(array);
         System.out.println(finalNumber);
     }
 
@@ -71,6 +71,7 @@ public class Main {
     }
 
     public static int marinisaac1Solution(int[] a) {
+//        int[] array = {1, 1, 2, 2, 1};
         for(int i = 0; i < a.length; i++)
             if(a[Math.abs(a[i]) - 1] < 0)
                 return Math.abs(a[i]);
