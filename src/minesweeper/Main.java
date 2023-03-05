446df54bdbda2cd05cbfa75b3230de503d3c1137
uchario <uchario@yahoo.com>
2023-03-06 00:16:58 +0100

    feat: add "minesweeper", "margaret_m1" and "jocke93" methods

--- src/minesweeper/Main.java
+++ src/minesweeper/Main.java
@@ -115,4 +115,21 @@ public class Main {
 
         return result;
     }
+
+    public static int[][] jocke93(boolean[][] matrix) {
+        int[][] m = new int[matrix.length][];
+        for(int i=0; i<matrix.length; i++)
+            m[i] = new int [matrix[i].length];
+
+        for(int i=0; i<matrix.length; i++)
+            for(int j=0; j<matrix[0].length; j++){
+                for(int k=i-1; k<i+2; k++)
+                    for(int l=j-1; l<j+2; l++)
+                        if(k>=0 && k<matrix.length && l>=0 && l<matrix[i].length &&
+                                !(k==i && j==l) && matrix[k][l])
+                            m[i][j]++;
+            }
+
+        return m;
+    }
 }
