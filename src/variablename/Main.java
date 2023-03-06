691c07b9bca80c9b41e7ef13f66b5a7f20f0abfb
uchario <uchario@yahoo.com>
2023-03-06 14:50:01 +0100

    feat: add problem statement

--- src/variablename/Main.java
+++ src/variablename/Main.java
@@ -6,6 +6,44 @@ public class Main {
     }
 
     public static boolean variableName(String name) {
+
+//        orrect variable names consist only of English letters, digits and underscores and they can't start with a digit.
+//
+//        Check if the given string is a correct variable name.
+//
+//        Example
+//
+//        For name = "var_1__Int", the output should be
+//        solution(name) = true;
+//        For name = "qq-q", the output should be
+//        solution(name) = false;
+//        For name = "2w2", the output should be
+//        solution(name) = false.
+//                Input/Output
+//
+//                [execution time limit] 3 seconds (java)
+//
+//                [input] string name
+//
+//        Guaranteed constraints:
+//        1 ≤ name.length ≤ 10.
+//
+//                [output] boolean
+//
+//        true if name is a correct variable name, false otherwise.
+//
+//[Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+//
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+//        String helloWorld(String name) {
+//            System.out.println("This prints to the console when you Run Tests");
+//            return "Hello, " + name;
+//        }
+
         if (Character.isDigit(name.charAt(0))) {
             return false;
         }
