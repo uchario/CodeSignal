4f46f00f694dbc300d6ee739b11293184c651976
uchario <uchario@yahoo.com>
2023-03-21 12:29:53 +0100

    feat: add description to "differentSymbolsNaive" method

--- src/differentsymbolsnaive/Main.java
+++ src/differentsymbolsnaive/Main.java
@@ -10,6 +10,40 @@ public class Main {
     }
 
     public static int differentSymbolsNaive(String s) {
+//        Given a string, find the number of different characters in it.
+//
+//                Example
+//
+//        For s = "cabca", the output should be
+//        solution(s) = 3.
+//
+//        There are 3 different characters a, b and c.
+//
+//        Input/Output
+//
+//                [execution time limit] 3 seconds (java)
+//
+//                [input] string s
+//
+//        A string of lowercase English letters.
+//
+//        Guaranteed constraints:
+//        3 ≤ s.length ≤ 1000.
+//
+//                [output] integer
+//
+//                [Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+//
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+//        String helloWorld(String name) {
+//            System.out.println("This prints to the console when you Run Tests");
+//            return "Hello, " + name;
+//        }
+
         Set<Character> alpha = new HashSet<>();
         for(int i = 0; i < s.length(); i++) {
             alpha.add(s.charAt(i));
