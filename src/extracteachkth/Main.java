688395936cfda27e2acdeec13a96dac354b0293b
uchario <uchario@yahoo.com>
2023-03-21 12:33:52 +0100

    feat: add description to "extractEachKth" method

--- src/extracteachkth/Main.java
+++ src/extracteachkth/Main.java
@@ -12,6 +12,44 @@ public class Main {
     }
 
     public static int[] extractEachKth(int[] inputArray, int k) {
+//        Given array of integers, remove each kth element from it.
+//
+//                Example
+//
+//        For inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] and k = 3, the output should be
+//        solution(inputArray, k) = [1, 2, 4, 5, 7, 8, 10].
+//
+//        Input/Output
+//
+//                [execution time limit] 3 seconds (java)
+//
+//                [input] array.integer inputArray
+//
+//        Guaranteed constraints:
+//        5 ≤ inputArray.length ≤ 15,
+//                -20 ≤ inputArray[i] ≤ 20.
+//
+//                [input] integer k
+//
+//        Guaranteed constraints:
+//        1 ≤ k ≤ 10.
+//
+//                [output] array.integer
+//
+//        inputArray without elements k - 1, 2k - 1, 3k - 1 etc.
+//
+//[Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+//
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+//        String helloWorld(String name) {
+//            System.out.println("This prints to the console when you Run Tests");
+//            return "Hello, " + name;
+//        }
+
         ArrayList<Integer> arrayList = new ArrayList<>();
         for(int i = 1; i <= inputArray.length; i++ ) {
             if(i % k != 0) {
