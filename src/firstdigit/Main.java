ad44af36dc8a25c3c5fdd766d187c607676d78c4
uchario <uchario@yahoo.com>
2023-03-21 12:31:31 +0100

    feat: add description to "firstDigit" method

--- src/firstdigit/Main.java
+++ src/firstdigit/Main.java
@@ -6,6 +6,41 @@ public class Main {
     }
 
     public static char firstDigit(String inputString) {
+//        Find the leftmost digit that occurs in a given string.
+//
+//        Example
+//
+//        For inputString = "var_1__Int", the output should be
+//        solution(inputString) = '1';
+//        For inputString = "q2q-q", the output should be
+//        solution(inputString) = '2';
+//        For inputString = "0ss", the output should be
+//        solution(inputString) = '0'.
+//                Input/Output
+//
+//                [execution time limit] 3 seconds (java)
+//
+//                [input] string inputString
+//
+//        A string containing at least one digit.
+//
+//                Guaranteed constraints:
+//        3 ≤ inputString.length ≤ 10.
+//
+//                [output] char
+//
+//[Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+//
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+//        String helloWorld(String name) {
+//            System.out.println("This prints to the console when you Run Tests");
+//            return "Hello, " + name;
+//        }
+
         char output = 'a';
         for(int i = 0; i < inputString.length(); i++) {
             if(Character.isDigit(inputString.charAt(i))) {
