46b3b2a5e4aa3d6722bdc11430ad2b7966a69508
uchario <uchario@yahoo.com>
2023-03-31 20:50:07 +0100

    feat: add "sumUpNumbers" method

---
+++ src/sumupnumbers/Main.java
@@ -0,0 +1,50 @@
+package sumupnumbers;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputString = "2 apples, 12 oranges";
+        System.out.println(sumUpNumbers(inputString));
+    }
+
+    public static int sumUpNumbers(String inputString) {
+//        CodeMaster has just returned from shopping. He scanned the check of the items he bought and gave the resulting string to Ratiorg to figure out the total number of purchased items. Since Ratiorg is a bot he is definitely going to automate it, so he needs a program that sums up all the numbers which appear in the given input.
+//
+//        Help Ratiorg by writing a function that returns the sum of numbers that appear in the given inputString.
+//
+//        Example
+//
+//        For inputString = "2 apples, 12 oranges", the output should be
+//        solution(inputString) = 14.
+//
+//        Input/Output
+//
+//                [execution time limit] 3 seconds (java)
+//
+//                [input] string inputString
+//
+//        Guaranteed constraints:
+//        0 ≤ inputString.length ≤ 105.
+//
+//                [output] integer
+//
+//                [Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+//
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+//        String helloWorld(String name) {
+//            System.out.println("This prints to the console when you Run Tests");
+//            return "Hello, " + name;
+
+        String[] splitInputString = inputString.split("\\D");
+        int sum = 0;
+        for(int i = 0; i < splitInputString.length; i++) {
+            if(!splitInputString[i].isBlank()) {
+                sum += Integer.parseInt(splitInputString[i]);
+            }
+        }
+        return sum;
+    }
+}
