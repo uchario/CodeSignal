fdeb34086ed55bb6f1de87fdcb425757d0c29be5
uchario <uchario@yahoo.com>
2023-04-08 23:18:47 +0100

    feat: add comment to "largestNumber" method

--- src/thecore/largestnumber/Main.java
+++ src/thecore/largestnumber/Main.java
@@ -7,6 +7,38 @@ public class Main {
     }
 
     public static int largestNumber(int n) {
+//        Given an integer n, return the largest number that contains exactly n digits.
+//
+//                Example
+//
+//        For n = 2, the output should be
+//        solution(n) = 99.
+//
+//        Input/Output
+//
+//                [execution time limit] 3 seconds (java)
+//
+//                [input] integer n
+//
+//        Guaranteed constraints:
+//        1 ≤ n ≤ 9.
+//
+//                [output] integer
+//
+//        The largest integer of length n.
+//
+//[Java] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+//
+// Globals declared here will cause a compilation error,
+// declare variables inside the function instead!
+//        String helloWorld(String name) {
+//            System.out.println("This prints to the console when you Run Tests");
+//            return "Hello, " + name;
+//        }
+
         String newString = "";
         for(int i = 0; i < n; i++) {
             newString += "9";
